f29f9bc7234d2a5be4fefa3c0b34da8710f4904e
Rylan Dirksen <rylandirksen@gmail.com>
2013-05-28 10:18:23 -0500

    game rules refactor

--- lib/game_rules.exs
+++ lib/game_rules.exs
@@ -5,19 +5,32 @@ defmodule GameRules do
   end
   
   def board_combinations(board) do
-    Enum.map( winning_combinations, fn(x) -> combine_winning_combination_and_board(x, board) end )
+    Enum.map( winning_combinations, fn(winning_combination) -> combine_winning_combination_and_board(winning_combination, board) end )
   end
   
   def combine_winning_combination_and_board(winning_combination, board) do
-    Enum.map( winning_combination, fn(x) -> Enum.fetch!(board, x) end )
+    Enum.map( winning_combination, fn(combination_index) -> Enum.fetch!(board, combination_index) end )
   end
   
   def winning_row_is_available_for_marker(board, marker) do
-    Enum.count( Enum.map( board_combinations(board), fn(x) -> x == [marker, marker, marker] end ), fn(x) -> x == true end ) > 0
+    Enum.count( Enum.map( board_combinations(board), fn(board_combination) -> board_combination == [marker, marker, marker] end ), fn(x) -> x == true end ) > 0
   end
   
   def is_winning_row( board_row ) do
     Enum.count( board_row, fn(x) -> x == :x end ) == 3
+  end
+  
+  def game_is_over(board, markers) do
+    Enum.count( markers, fn(marker) -> winning_row_is_available_for_marker(board, marker) == true end ) > 0 
+      || game_is_tie(board)
+  end
+  
+  def game_is_tie(board) do
+    count_marker_type(board, :_) == 0
+  end
+  
+  def count_marker_type(board, marker) do
+    Enum.count( board, fn(board_space) -> board_space == marker end )
   end
   
 end
--- test/game_rules_test.exs
+++ test/game_rules_test.exs
@@ -6,7 +6,9 @@ defmodule GameRulesTest do
   use ExUnit.Case
   
   test "combines the given board with the winning combinations" do
-    assert GameRules.board_combinations( [:x,:x,:x,:_,:_,:o,:_,:_,:o] ) == [[:x,:x,:x],[:_,:_,:o],[:_,:_,:o],[:x,:_,:_],[:x,:_,:_],[:x,:o,:o],[:x,:_,:o],[:x,:_,:_]]
+    assert GameRules.board_combinations( [:x,:x,:x,
+                                          :_,:_,:o,
+                                          :_,:_,:o] ) == [[:x,:x,:x],[:_,:_,:o],[:_,:_,:o],[:x,:_,:_],[:x,:_,:_],[:x,:o,:o],[:x,:_,:o],[:x,:_,:_]]
   end
   
   test "the board has spaces" do
@@ -15,12 +17,28 @@ defmodule GameRulesTest do
   end
   
   test "returns true if winning row is present" do
-    assert GameRules.winning_row_is_available_for_marker( [:x,:x,:x,:_,:_,:_,:_,:_,:_], :x ) == true
-    assert GameRules.winning_row_is_available_for_marker( [:x,:x,:x,:_,:_,:_,:_,:_,:_], :o ) == false
+    assert GameRules.winning_row_is_available_for_marker( [:x,:x,:x,
+                                                           :_,:_,:_,
+                                                           :_,:_,:_], :x ) == true
+    assert GameRules.winning_row_is_available_for_marker( [:x,:x,:x,
+                                                           :_,:_,:_,
+                                                           :_,:_,:_], :o ) == false
   end
   
   test "returns winning row combinations" do
     assert GameRules.winning_combinations == [ [0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6] ]
+  end
+  
+  test "game is over conditions" do
+    assert GameRules.game_is_over( [:x,:x,:x,
+                                    :_,:_,:_,
+                                    :_,:_,:_], [:x, :o] ) == true
+    assert GameRules.game_is_over( [:x,:x,:o,
+                                    :_,:_,:_,
+                                    :_,:_,:_], [:x, :o] ) == false
+    assert GameRules.game_is_over( [:x,:o,:x,
+                                    :o,:x,:o,
+                                    :o,:x,:o], [:x, :o] ) == true
   end
   
 end
